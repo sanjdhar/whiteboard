5c33699d2a6d1e3b224265bcad237d28dc83d004
Sanjay Dhar <sanjaydhar@hotmail.com>
2026-04-18 18:44:35 -0400

    Updated page.tsx to add title to the html tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import Whiteboard from "./components/Whiteboard";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Simple Online Whiteboard",
+  description: "Simple Online Whiteboard",
+};
 
 export default function Home() {
   return <Whiteboard />;
